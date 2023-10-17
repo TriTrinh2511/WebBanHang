6d04a26ad7fd5675605476d277cb87e6f55f1c33
MSI <MSI@DESKTOP-GVJB3MN>
2023-10-17 17:00:50 +0700

    Add project files.

---
+++ WebBanHang/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHang.Models
+{
+    public partial class Account
+    {
+        public int AccountId { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? PassWord { get; set; }
+        public string Active { get; set; } = null!;
+        public string? FullName { get; set; }
+        public int RoleId { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? CreateDate { get; set; }
+
+        public virtual Role Role { get; set; } = null!;
+    }
+}
---
+++ WebBanHang/Models/Website_EcomerceContext.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebBanHang.Models
+{
+    public partial class Website_EcomerceContext : DbContext
+    {
+        public Website_EcomerceContext()
+        {
+        }
+
+        public Website_EcomerceContext(DbContextOptions<Website_EcomerceContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Location> Locations { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Page> Pages { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+        public virtual DbSet<Shipper> Shippers { get; set; } = null!;
+        public virtual DbSet<TinTuc> TinTucs { get; set; } = null!;
+        public virtual DbSet<TransactStatus> TransactStatuses { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-GVJB3MN\\SQLEXPRESS;Database=Website_Ecomerce;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.AccountId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("AccountID");
+
+                entity.Property(e => e.Active)
+                    .HasMaxLength(6)
+                    .IsFixedLength();
+
+                entity.Property(e => e.CreateDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FullName).HasMaxLength(150);
+
+                entity.Property(e => e.LastLogin).HasColumnType("datetime");
+
+                entity.Property(e => e.PassWord)
+                    .HasMaxLength(50)
+                    .HasColumnName("Pass_word");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(12)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Accounts)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_Accounts");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CatId)
+                    .HasName("pk_Categories");
+
+                entity.Property(e => e.CatId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CatID");
+
+                entity.Property(e => e.Alias).HasMaxLength(250);
+
+                entity.Property(e => e.CatName).HasMaxLength(250);
+
+                entity.Property(e => e.Cover).HasMaxLength(250);
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(250);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(250);
+
+                entity.Property(e => e.ParentId).HasColumnName("ParentID");
+
+                entity.Property(e => e.Thumb).HasMaxLength(250);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.CustomerId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CustomerID");
+
+                entity.Property(e => e.Addressed).HasMaxLength(255);
+
+                entity.Property(e => e.Avatar).HasMaxLength(255);
+
+                entity.Property(e => e.Birthday).HasColumnType("datetime");
+
+                entity.Property(e => e.CreateDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(150)
+                    .IsFixedLength();
+
+                entity.Property(e => e.FullName).HasMaxLength(255);
+
+                entity.Property(e => e.LastLogin).HasColumnType("datetime");
+
+                entity.Property(e => e.LocationId).HasColumnName("LocationID");
+
+                entity.Property(e => e.PassWord)
+                    .HasMaxLength(50)
+                    .HasColumnName("Pass_word");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(12)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salt)
+                    .HasMaxLength(8)
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Location>(entity =>
+            {
+                entity.Property(e => e.LocationId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("LocationID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("Name_");
+
+                entity.Property(e => e.NameWithType).HasMaxLength(255);
+
+                entity.Property(e => e.PathWithType).HasMaxLength(255);
+
+                entity.Property(e => e.Slug).HasMaxLength(100);
+
+                entity.Property(e => e.Type)
+                    .HasMaxLength(20)
+                    .HasColumnName("Type_");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("OrderID");
+
+                entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+
+                entity.Property(e => e.OrderDate).HasColumnType("datetime");
+
+                entity.Property(e => e.PaymentDate).HasColumnType("datetime");
+
+                entity.Property(e => e.PaymentId).HasColumnName("PaymentID");
+
+                entity.Property(e => e.ShipDate).HasColumnType("datetime");
+
+                entity.Property(e => e.TransactStatusId).HasColumnName("TransactStatusID");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("fk_Orders_Customer");
+
+                entity.HasOne(d => d.TransactStatus)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.TransactStatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_Orders_Status");
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.Property(e => e.OrderDetailId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("OrderDetailID");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.ShipDate).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .HasConstraintName("fk_OrderDetails");
+            });
+
+            modelBuilder.Entity<Page>(entity =>
+            {
+                entity.Property(e => e.PageId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("PageID");
+
+                entity.Property(e => e.Alias).HasMaxLength(250);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(250);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(250);
+
+                entity.Property(e => e.PageName).HasMaxLength(250);
+
+                entity.Property(e => e.Thumb).HasMaxLength(250);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ProductID");
+
+                entity.Property(e => e.Alias).HasMaxLength(255);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.DateCreated).HasColumnType("datetime");
+
+                entity.Property(e => e.DateModified).HasColumnType("datetime");
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(255);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(255);
+
+                entity.Property(e => e.ProductName).HasMaxLength(255);
+
+                entity.Property(e => e.ShortDesc).HasMaxLength(255);
+
+                entity.Property(e => e.Thumb).HasMaxLength(255);
+
+                entity.Property(e => e.Title).HasMaxLength(255);
+
+                entity.Property(e => e.Video).HasMaxLength(255);
+
+                entity.HasOne(d => d.Cat)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CatId)
+                    .HasConstraintName("fk_Products");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.RoleId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("RoleID");
+
+                entity.Property(e => e.Descriptions).HasMaxLength(50);
+
+                entity.Property(e => e.RoleName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Shipper>(entity =>
+            {
+                entity.Property(e => e.ShipperId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ShipperID");
+
+                entity.Property(e => e.Company).HasMaxLength(150);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.ShipDate).HasColumnType("datetime");
+
+                entity.Property(e => e.ShipperName).HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<TinTuc>(entity =>
+            {
+                entity.HasKey(e => e.PostId)
+                    .HasName("pk_PostID");
+
+                entity.Property(e => e.PostId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("PostID");
+
+                entity.Property(e => e.AccountId).HasColumnName("AccountID");
+
+                entity.Property(e => e.Alias).HasMaxLength(255);
+
+                entity.Property(e => e.Author).HasMaxLength(255);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.IsHot).HasColumnName("isHot");
+
+                entity.Property(e => e.IsNewfeed)
+                    .HasMaxLength(255)
+                    .HasColumnName("isNewfeed");
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(255);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(255);
+
+                entity.Property(e => e.Sconntents)
+                    .HasMaxLength(255)
+                    .HasColumnName("SConntents");
+
+                entity.Property(e => e.Thumb).HasMaxLength(255);
+
+                entity.Property(e => e.Title).HasMaxLength(255);
+
+                entity.Property(e => e.Views).HasColumnName("Views_");
+            });
+
+            modelBuilder.Entity<TransactStatus>(entity =>
+            {
+                entity.ToTable("TransactStatus");
+
+                entity.Property(e => e.TransactStatusId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("TransactStatusID");
+
+                entity.Property(e => e.Description).HasColumnName("Description_");
+
+                entity.Property(e => e.Status)
+                    .HasMaxLength(50)
+                    .HasColumnName("Status_");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
